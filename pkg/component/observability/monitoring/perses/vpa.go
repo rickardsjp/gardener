9c54dbd29a4ebfc42e89f97147a1117926075b3a
Jeremy Rickards <jeremy.rickards@sap.com>
2026-08-11 20:04:07 +0200

    Add VerticalPodAutoscaler for Perses
    Deploy a VerticalPodAutoscaler for the Perses instance when VPA is
    enabled on the cluster.
    
    Signed-off-by: Jeremy Rickards <jeremy.rickards@sap.com>

---
+++ pkg/component/observability/monitoring/perses/vpa.go
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package perses
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
+	"k8s.io/utils/ptr"
+)
+
+func (p *perses) vpa() *vpaautoscalingv1.VerticalPodAutoscaler {
+	if !p.values.VPAEnabled {
+		return nil
+	}
+
+	return &vpaautoscalingv1.VerticalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      p.persesName(),
+			Namespace: p.namespace,
+			Labels:    p.getLabels(),
+		},
+		Spec: vpaautoscalingv1.VerticalPodAutoscalerSpec{
+			TargetRef: &autoscalingv1.CrossVersionObjectReference{
+				APIVersion: appsv1.SchemeGroupVersion.String(),
+				Kind:       "Deployment",
+				Name:       p.persesName(),
+			},
+			UpdatePolicy: &vpaautoscalingv1.PodUpdatePolicy{
+				UpdateMode: ptr.To(vpaautoscalingv1.UpdateModeAuto),
+			},
+			ResourcePolicy: &vpaautoscalingv1.PodResourcePolicy{
+				ContainerPolicies: []vpaautoscalingv1.ContainerResourcePolicy{
+					{
+						ContainerName: "perses",
+						MinAllowed: corev1.ResourceList{
+							corev1.ResourceMemory: resource.MustParse("32Mi"),
+						},
+						ControlledValues: ptr.To(vpaautoscalingv1.ContainerControlledValuesRequestsOnly),
+					},
+					{
+						ContainerName: vpaautoscalingv1.DefaultContainerResourcePolicy,
+						Mode:          ptr.To(vpaautoscalingv1.ContainerScalingModeOff),
+					},
+				},
+			},
+		},
+	}
+}
